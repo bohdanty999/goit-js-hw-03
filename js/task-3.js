2706d2305d77e8a4a322291b72b14256332c3885
bohdanty999 <bohdanty1@gmail.com>
2026-03-31 19:44:05 +0100

    typo fix

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ function filterArray(numbers, value) {
   const newArray = [];
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > value) {
-      newArray.push(number[i]);
+      newArray.push(numbers[i]);
     }
   }
   return newArray;
